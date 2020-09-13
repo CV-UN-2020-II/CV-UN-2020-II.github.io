53f670e0c737675d3c650e00f82cfc19a323753a
Andres Roman <afromana@unal.edu.co>
2020-09-13 11:54:10 -0500

    Little improve

--- src/components/p5.js
+++ src/components/p5.js
@@ -1,14 +1,12 @@
 import React, { Component } from "react"
 import Loadable from "@loadable/component";
-import Sketch from "./sketch";
 
 const LoadableP5 = Loadable(() => import('react-p5-wrapper'));
 
 export default class App extends Component{
   render() {
     return( 
-        <LoadableP5 sketch={Sketch}/>
-       
+        <LoadableP5 sketch={this.props.sk}/>
     )
   }
 }
--- src/pages/prueba.js
+++ src/pages/prueba.js
@@ -1,11 +1,12 @@
   
 import React from "react";
 import P5 from "../components/p5";
+import Sketch from "../components/sketch";
 import Layout from '@rocketseat/gatsby-theme-docs/src/components/Layout';
 
 const prueba = () => (
     <Layout>
-        <P5/>
+        <P5 sk={Sketch}/>
     </Layout>
 
   )
