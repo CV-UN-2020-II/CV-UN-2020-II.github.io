c0a8adcf25638800882a8943012ac885e07b8112
Andres Roman <afromana@unal.edu.co>
2020-09-17 10:16:51 -0500

    Load image

--- src/pages/prueba3.js
+++ src/pages/prueba3.js
@@ -5,6 +5,7 @@ import Layout from '@rocketseat/gatsby-theme-docs/src/components/Layout';
 
 const prueba3 = () => (
     <Layout>
+        <h1>Hola</h1>
         <Wraper sk={Sketch}/>
     </Layout>
 )
