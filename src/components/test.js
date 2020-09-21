44184d90b1a074cf3896ecdd8153bd5d45705d73
Andres Roman <afromana@unal.edu.co>
2020-09-21 07:14:07 -0500

    Select and image with effect

--- src/components/test.js
+++ src/components/test.js
@@ -1,47 +1,65 @@
 export default function Test( p5 ) {
-
-    var input;
     var img;
-    var leftBuffer;
-    var centerBuffer;
-    var rightBuffer;
+    var copia;
+    var sel;
 
     p5.setup=()=> {
 
-        input = p5.createFileInput(p5.handleFile);
+        p5.createFileInput(p5.handleFile);
+        sel = p5.createSelect();
+        sel.option('...','0');
+        sel.option('Promedio RGB','1');
+        sel.option('Luma','2');
+        sel.option('Efecto borroso','3');
+        sel.option('Deteccion de bordes','4');
+        sel.option('Histograma','5');
+        sel.option('Segmentacion','6');
+        sel.selected('0');
+        sel.changed(change)
         p5.createCanvas(800, 400);
-        leftBuffer = p5.createGraphics(350, 400);
-        centerBuffer = p5.createGraphics(100,400);
-        rightBuffer = p5.createGraphics(350, 400);
+    }
+
+    function change(){
+        let val=sel.value();
+        if(val=='1'){
+            if(img){
+                drawRGB();
+            }
+        }else if(val=='2'){
+            
+        }
+    }
+
+    
+   function drawRGB(){
+        img.loadPixels();
+        copia=p5.createImage(img.width, img.height)
+        copia.loadPixels();
+        for (var y = 0; y < img.height; y++) {
+            for (var x = 0; x < img.width; x++) {
+              var index = (x + y * img.width)*4;
+              copia.pixels[index+0] = img.pixels[index+1]
+              copia.pixels[index+1] = img.pixels[index+0]
+              copia.pixels[index+2] = img.pixels[index+2]
+              copia.pixels[index+3] = img.pixels[index+3]     
+            }
+          }
+        copia.updatePixels();
+        p5.image(copia, 450, 0, 350, 400);
     }
 
     p5.draw=()=> {
-        drawRightBuffer();
-        drawCenterBuffer();
-        p5.image(rightBuffer, 450, 0);
-        p5.image(centerBuffer,300,0);
         if (img) {
-           p5.image(img, 0, 0, leftBuffer.width, leftBuffer.height);
+           p5.image(img, 0, 0, 350, 400);
         }
-    }
-    
-    function drawCenterBuffer(){
-        centerBuffer.background(255, 255, 255);
-        centerBuffer.fill(0,0,0);
     }
 
-   function drawRightBuffer(){
-        if(img){
-            p5.image(img,450,0,rightBuffer.width,rightBuffer.height);
-            p5.filter(p5.GRAY)
-        }
-    }
 
     p5.handleFile=(file)=> {
-        p5.print(file);
+        //p5.print(file);
         if (file.type === 'image') {
-            img = p5.createImg(file.data, '');
-            img.hide();
+            img = p5.loadImage(file.data, '');
+            p5.print(img.width)   //Parece que p5js fuera asincrono inside function
         } else {
             img = null;
         }
