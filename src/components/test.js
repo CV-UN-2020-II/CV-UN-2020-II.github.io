718c2998ef394526e303b2d0492f1b92b950ccda
Andres Roman <afromana@unal.edu.co>
2020-09-24 13:52:51 -0500

    Convolution optimization

--- src/components/test.js
+++ src/components/test.js
@@ -31,9 +31,9 @@ export default function Test( p5 ) {
                 drawRGBLuma(val)
             }
         }else if(val=='3'){
-            let v=1/9
-            var matr=[[v,v,v],[v,v,v],[v,v,v]]
-            drawConvolution(matr)
+            let v=1
+            var k1=[[v,v,v],[v,v,v],[v,v,v]]
+            drawConvolution(k1)
         }else if(val=='4'){
             let v=-1
             var matr=[[v,v,v],[v,-8,v],[v,v,v]]
@@ -42,28 +42,61 @@ export default function Test( p5 ) {
     }
 
     
-    function drawConvolution(matr){
+    function drawConvolution(k1){
         img.loadPixels();
         copia=p5.createImage(img.width,img.height);
         copia.loadPixels()
-        for(let x=1;x<img.width;x++){
-            for(let y=1;y<img.height;y++){
-               
-                let sumR=0,sumG=0,sumB=0;
-                for(let kx=-1;kx<2;kx++){
-                    for(let ky=-1;ky<2;ky++){
-                        let posx=x+kx;
-                        let posy=y+ky;
-                        let val1= p5.red(img.get(posx,posy))
-                        let val2= p5.green(img.get(posx,posy))
-                        let val3= p5.blue(img.get(posx,posy))
-
-                        sumR+=matr[kx+1][ky+1]*val1
-                        sumG+=matr[kx+1][ky+1]*val2
-                        sumB+=matr[kx+1][ky+1]*val3
-                    }
-                }
-                copia.set(x,y,p5.color(sumR,sumG,sumB))
+        var h=img.height;
+        var w=img.width;
+        for(let x=0;x<w;x++){
+            for(let y=0;y<h;y++){
+                var ul = ((x-1+w)%w + w*((y-1+h)%h))*4; 
+				var uc = ((x-0+w)%w + w*((y-1+h)%h))*4; 
+				var ur = ((x+1+w)%w + w*((y-1+h)%h))*4; 
+				var ml = ((x-1+w)%w + w*((y+0+h)%h))*4; 
+				var mc = ((x-0+w)%w + w*((y+0+h)%h))*4; 
+				var mr = ((x+1+w)%w + w*((y+0+h)%h))*4; 
+				var ll = ((x-1+w)%w + w*((y+1+h)%h))*4; 
+				var lc = ((x-0+w)%w + w*((y+1+h)%h))*4; 
+                var lr = ((x+1+w)%w + w*((y+1+h)%h))*4;
+                
+                var p0 = img.pixels[ul]*k1[0][0]; 
+                var p1 = img.pixels[uc]*k1[0][1]; 
+                var p2 = img.pixels[ur]*k1[0][2]; 
+				var p3 = img.pixels[ml]*k1[1][0]; 
+				var p4 = img.pixels[mc]*k1[1][1]; 
+				var p = img.pixels[mr]*k1[1][2]; 
+				var p6 = img.pixels[ll]*k1[2][0]; 
+				var p7 = img.pixels[lc]*k1[2][1]; 
+				var p8 = img.pixels[lr]*k1[2][2]; 
+				var red = (p0+p1+p2+p3+p4+p+p6+p7+p8)/9;
+					
+                p0 = img.pixels[ul+1]*k1[0][0]; 
+                p1 = img.pixels[uc+1]*k1[0][1]; 
+				p2 = img.pixels[ur+1]*k1[0][2]; 
+				p3 = img.pixels[ml+1]*k1[1][0]; 
+				p4 = img.pixels[mc+1]*k1[1][1]; 
+				p = img.pixels[mr+1]*k1[1][2]; 
+				p6 = img.pixels[ll+1]*k1[2][0]; 
+				p7 = img.pixels[lc+1]*k1[2][1]; 
+				p8 = img.pixels[lr+1]*k1[2][2];
+				var green = (p0+p1+p2+p3+p4+p+p6+p7+p8)/9;
+					
+				p0 = img.pixels[ul+2]*k1[0][0];
+				p1 = img.pixels[uc+2]*k1[0][1];
+				p2 = img.pixels[ur+2]*k1[0][2];
+                p3 = img.pixels[ml+2]*k1[1][0];
+                p4 = img.pixels[mc+2]*k1[1][1]; 
+				p = img.pixels[mr+2]*k1[1][2]; 
+				p6 = img.pixels[ll+2]*k1[2][0];
+				p7 = img.pixels[lc+2]*k1[2][1];
+				p8 = img.pixels[lr+2]*k1[2][2];
+				var blue = (p0+p1+p2+p3+p4+p+p6+p7+p8)/9;
+					
+				copia.pixels[mc] = red;
+				copia.pixels[mc+1] = green;
+				copia.pixels[mc+2] = blue;
+				copia.pixels[mc+3] = img.pixels[lc+3];      
             }
         }
         copia.updatePixels()
@@ -95,11 +128,6 @@ export default function Test( p5 ) {
         copia.updatePixels();
         p5.image(copia, 450, 0, 350, 400);
     }
-
-
-
-    
-   
 
     p5.draw=()=> {
         if (img) {
