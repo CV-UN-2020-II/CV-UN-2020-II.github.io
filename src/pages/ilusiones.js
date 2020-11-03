02ef8fae18ebfb5f005ca0dbcd60cdb3ed623028
Yarid Perez <ayperezc@unal.edu.co>
2020-11-03 08:47:30 -0500

    dos ilusiones mas

--- src/pages/ilusiones.js
+++ src/pages/ilusiones.js
@@ -2,6 +2,8 @@ import React from "react";
 import Wraper from "../components/wrapper";
 import ilusion1 from "../components/ilusion1";
 import ilusion2 from "../components/ilusion2";
+import ilusion3 from "../components/ilusion3";
+import ilusion4 from "../components/ilusion4";
 import Layout from '@rocketseat/gatsby-theme-docs/src/components/Layout';
 
 const ilusiones=()=>(
@@ -23,6 +25,16 @@ const ilusiones=()=>(
            </p>
            <span>Nota: </span> Click para quitar o poner los cuadrados
            <Wraper sk={ilusion2}/>
+           <h2><li>Breathing Square Illusion</li></h2>
+           <p>
+            Visualmente, el cuadrado escondido parece estar haciéndose más grande o más pequeño, pero solo está rotando. Presione IZQUIERDA para ocultar los cuadrados naranjas y DERECHA para mostrarlos. Tomado de https://michaelbach.de/ot/mot-breathingSquare/index.html
+           </p>
+           <Wraper sk={ilusion3}/>
+           <h2><li>Stepping feet Illusion</li></h2>
+           <p>
+            Los dos rectángulos se mueven igual, pero pareciera que se mueven de a pasos. Presione ESPACIO para quitar las franjas. Tomado de https://michaelbach.de/ot/mot-feetLin/index.html
+           </p>
+           <Wraper sk={ilusion4}/>
        </ol>
       
     </Layout>
