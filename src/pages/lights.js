599ca7e839b26ffdd37151b8f1936be9014222dc
Guiselle Tatiana Zambrano Penagos <gtzambranop@unal.edu.co>
2020-11-25 17:53:30 -0500

    demo version

---
+++ src/pages/lights.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+import Scene from  "../components/scene";
+import Layout from '@rocketseat/gatsby-theme-docs/src/components/Layout';
+
+const IndexPage = () => (
+  <Layout>
+    <Scene />
+  </Layout>
+)
+
+export default IndexPage
